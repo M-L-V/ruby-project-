c698acec8f86ff15ce35a948ebad0a70ba6b53ea
Morgaine <morgainedv@gmail.com>
2019-11-11 10:03:47 +0000

    add controllers

---
+++ controllers/publishers_controller.rb
@@ -0,0 +1,7 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+require_relative('../models/publisher.rb')
+require_relative('../models/book.rb')
+
+also_reload('../models/*')
